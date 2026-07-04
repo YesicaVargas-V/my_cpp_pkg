f5b4990c53bb5c5efd4a3d29e2f77e8354f7759b
Yesica Velandia <jeveva2002@gmail.com>
2026-07-04 12:23:44 -0500

    The actualization are for the new activities

--- src/number_counter.cpp
+++ src/number_counter.cpp
@@ -1,5 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/int64.hpp"
+#include "example_interfaces/srv/set_bool.hpp"
 
 using namespace std::placeholders;
 
@@ -11,22 +12,45 @@ public:
         counter_publisher_ = this-> create_publisher<example_interfaces::msg::Int64>("number_count", 10);
         number_susbcriber_ = this-> create_subscription<example_interfaces::msg::Int64>("number", 10,
                         std::bind(&NumberCounterNode::callbackNumberCounter, this, std::placeholders::_1));
+        server_ = this->create_service<example_interfaces::srv::SetBool>("reset_counter", 
+                        std::bind(&NumberCounterNode::callbackResetCounter, this, _1 ,_2));
+
         RCLCPP_INFO(this->get_logger(), "Number counter hass been started");
     }
 
 private:
-    void callbackNumberCounter(const example_interfaces::msg::Int64::SharedPtr msg){
-
+    void callbackNumberCounter(const example_interfaces::msg::Int64::SharedPtr msg)
+    {
         counter = msg->data + counter;
         auto newMsg = example_interfaces::msg::Int64();
         newMsg.data = counter;
         counter_publisher_->publish(newMsg);
+    }
+    void callbackResetCounter( const example_interfaces::srv::SetBool::Request::SharedPtr request,
+                               const example_interfaces::srv::SetBool::Response::SharedPtr response)
+    {
+        if (request->data ==true)
+        {
+            counter = 0;
+            response->success = true;
+            response->message = "The counter is restarted to 0";
 
+        }
+        else
+        {
+            counter = counter;
+            response->success = false;
+            response->message = "The counter stay the same";
+
+        }
 
     }
+
+
     int counter; 
     rclcpp::Subscription<example_interfaces::msg::Int64>::SharedPtr number_susbcriber_;
     rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr counter_publisher_; 
+    rclcpp::Service<example_interfaces::srv::SetBool>::SharedPtr server_;
 
 };
 
@@ -38,3 +62,4 @@ int main(int argc, char **argv)
     rclcpp::shutdown();
     return 0;
 } 
+
